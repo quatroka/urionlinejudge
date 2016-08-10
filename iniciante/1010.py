5c99d51b24de7235b0e47f3bf87bab9ec7dc2bda
quatroka <monteiro.leonardosantos@gmail.com>
2016-08-10 02:08:23 -0300

    Python -> Iniciante -> 1010 -> Completado

---
+++ iniciante/1010.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+p1 = input()
+p1 = p1.split()
+p2 = input()
+p2 = p2.split()
+totalp1 = int(p1[1]) * float(p1[2])
+totalp2 = int(p2[1]) * float(p2[2])
+
+print('VALOR A PAGAR: R$ %.2f' % (totalp1 + totalp2))
