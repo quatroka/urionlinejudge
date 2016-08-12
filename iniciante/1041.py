88b69d1de8770c08beb6475cd048da255c4964f7
quatroka <monteiro.leonardosantos@gmail.com>
2016-08-12 11:15:52 -0300

    Python -> Iniciante -> 1041 -> Completado

---
+++ iniciante/1041.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+line = input()
+line = line.split()
+
+x = float(line[0])
+y = float(line[1])
+
+if(x == 0 and y == 0):
+    print('Origem')
+
+if(x == 0 and y != 0):
+    print('Eixo Y')
+
+if(x != 0 and y == 0):
+    print('Eixo X')
+
+if(x > 0 and y > 0):
+    print('Q1')
+
+if(x < 0 and y > 0):
+    print('Q2')
+
+if(x < 0 and y < 0):
+    print('Q3')
+
+if(x > 0 and y < 0):
+    print('Q4')
