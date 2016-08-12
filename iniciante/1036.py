b2b6278cc5651d3d4870f849cf842140b9c5b4ef
quatroka <monteiro.leonardosantos@gmail.com>
2016-08-12 01:23:54 -0300

    Python -> Iniciante -> 1036 -> Completado

---
+++ iniciante/1036.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+line = input()
+line = line.split()
+a = float(line[0])
+b = float(line[1])
+c = float(line[2])
+
+delta = b**2 - (4 * a * c)
+
+if(delta <= 0 or a == 0):
+    print('Impossivel calcular')
+else:
+    r1 = (-b + (delta**(1 / 2))) / (2 * a)
+    r2 = (-b - (delta**(1 / 2))) / (2 * a)
+    print('R1 = %.5f' % r1)
+    print('R2 = %.5f' % r2)
