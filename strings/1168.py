ad07ea55bde2ef6cf61dfdb8e7330ca3512a3bcd
quatroka <monteiro.leonardosantos@gmail.com>
2016-08-12 12:30:29 -0300

    Python -> Strings -> 1168 -> Completado

---
+++ strings/1168.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+casos = int(input())
+
+for i in range(0, casos):
+    num = str(input())
+    leds = 0
+    for j in range(0, len(num)):
+        if(num[j] == '1'):
+            leds += 2
+        elif(num[j] == '2' or num[j] == '3' or num[j] == '5'):
+            leds += 5
+        elif(num[j] == '4'):
+            leds += 4
+        elif(num[j] == '6' or num[j] == '9' or num[j] == '0'):
+            leds += 6
+        elif(num[j] == '7'):
+            leds += 3
+        elif(num[j] == '8'):
+            leds += 7
+
+    print('%d leds' % leds)
