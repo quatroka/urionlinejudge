08d6ee62aa6b26b631d0e4bd74ce0af322378438
quatroka <monteiro.leonardosantos@gmail.com>
2016-08-18 17:41:02 -0300

    Python -> Iniciante -> 1050 -> Completado

---
+++ iniciante/1050.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+line = int(input())
+
+if(line == 61):
+    print('Brasilia')
+elif(line == 71):
+    print('Salvador')
+elif(line == 11):
+    print('Sao Paulo')
+elif(line == 21):
+    print('Rio de Janeiro')
+elif(line == 32):
+    print('Juiz de Fora')
+elif(line == 19):
+    print('Campinas')
+elif(line == 27):
+    print('Vitoria')
+elif(line == 31):
+    print('Belo Horizonte')
+else:
+    print('DDD nao cadastrado')
