c5d18c8298de09d9ce511d130240e44be54c5db3
quatroka <monteiro.leonardosantos@gmail.com>
2016-08-18 05:35:13 -0300

    Python -> Iniciante -> 1052 -> Completado

---
+++ iniciante/1052.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+line = int(input())
+
+if(line == 1):
+    print('January')
+elif(line == 2):
+    print('February')
+elif(line == 3):
+    print('March')
+elif(line == 4):
+    print('April')
+elif(line == 5):
+    print('May')
+elif(line == 6):
+    print('June')
+elif(line == 7):
+    print('July')
+elif(line == 8):
+    print('August')
+elif(line == 9):
+    print('September')
+elif(line == 10):
+    print('October')
+elif(line == 11):
+    print('November')
+elif(line == 12):
+    print('December')
