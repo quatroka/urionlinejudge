3e18d9530b7e46d55fc0975311135c2e57cccdfa
quatroka <monteiro.leonardosantos@gmail.com>
2016-08-19 04:52:47 -0300

    Python -> Iniciante -> 1080 -> Completado

---
+++ iniciante/1080.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+posicao = 0
+maior = 0
+for i in range(1, 101):
+    n = int(input())
+    if(n > maior):
+        maior = n
+        posicao = i
+
+print(maior)
+print(posicao)
