3de15f39ce6355d97399460eb77a46d8bcd32e13
quatroka <monteiro.leonardosantos@gmail.com>
2016-08-19 02:50:47 -0300

    Python -> Iniciante -> 1064 -> Completado

---
+++ iniciante/1064.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+cont = 0
+media = 0
+for i in range(0, 6):
+    x = float(input())
+    if(x > 0):
+        media += x
+        cont += 1
+
+print('%s valores positivos' % cont)
+print('%.1f' % (media / cont))
