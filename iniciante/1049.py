da7c052b6036bdb7430025255fd49f02675a209d
quatroka <monteiro.leonardosantos@gmail.com>
2016-08-19 01:55:44 -0300

    Python -> Iniciante -> 1049 -> Completado

---
+++ iniciante/1049.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+subfilo = input()
+tipo = input()
+alimento = input()
+
+if(subfilo == 'vertebrado'):
+    if(tipo == 'ave'):
+        if(alimento == 'carnivoro'):
+            print('aguia')
+        elif(alimento == 'onivoro'):
+            print('pomba')
+    elif(tipo == 'mamifero'):
+        if(alimento == 'onivoro'):
+            print('homem')
+        elif(alimento == 'herbivoro'):
+            print('vaca')
+elif(subfilo == 'invertebrado'):
+    if(tipo == 'inseto'):
+        if(alimento == 'hematofago'):
+            print('pulga')
+        elif(alimento == 'herbivoro'):
+            print('lagarta')
+    elif(tipo == 'anelideo'):
+        if(alimento == 'hematofago'):
+            print('sanguessuga')
+        elif(alimento == 'onivoro'):
+            print('minhoca')
