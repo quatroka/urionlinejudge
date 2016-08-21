28be4d195a3e30bd4c8817a444768f3d729c3f7a
quatroka <monteiro.leonardosantos@gmail.com>
2016-08-21 01:49:59 -0300

    Python -> Iniciante -> 1098 -> Completado

---
+++ iniciante/1098.py
@@ -0,0 +1,13 @@
+from decimal import Decimal
+
+i = 0
+j = 1
+while(i <= 2):
+    if(i / 2 == 0 or i / 2 == 0.5 or i / 2 == 1):
+        print('I=%.0f J=%.0f' % (i, j))
+    else:
+        print('I={} J={}'.format(i, j))
+    j = j + 1
+    if(j == (i + 4)):
+        i = i + Decimal('0.2')
+        j = j - Decimal('2.8')
