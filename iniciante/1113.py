07611f136e67b0a1af070eef680c7d3587062ece
quatroka <monteiro.leonardosantos@gmail.com>
2016-08-22 01:55:03 -0300

    Python -> Iniciante -> 1113 -> Completado

---
+++ iniciante/1113.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+while(True):
+    line = input().split()
+    x = int(line[0])
+    y = int(line[1])
+    if(x == y):
+        break
+    elif(x < y):
+        print('Crescente')
+    elif(x > y):
+        print('Decrescente')
