b62a379a67ead588a19899b04774707c74b0f923
quatroka <monteiro.leonardosantos@gmail.com>
2016-08-22 02:05:54 -0300

    Python -> Iniciante -> 1115 -> Completado

---
+++ iniciante/1115.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+while(True):
+    line = input().split()
+    x = int(line[0])
+    y = int(line[1])
+
+    if(x == 0 or y == 0):
+        break
+    elif(x > 0 and y > 0):
+        print('primeiro')
+    elif(x < 0 and y > 0):
+        print('segundo')
+    elif(x < 0 and y < 0):
+        print('terceiro')
+    elif(x > 0 and y < 0):
+        print('quarto')
