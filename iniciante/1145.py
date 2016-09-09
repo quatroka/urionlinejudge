1d0a1209b6c08f712f97561309be8aff1fe752a8
quatroka <monteiro.leonardosantos@gmail.com>
2016-09-09 16:51:34 -0300

    Python -> Iniciante -> 1145 -> Completado

---
+++ iniciante/1145.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+line = input().split()
+x = int(line[0])
+y = int(line[1])
+
+count = 0
+for i in range(1, y + 1):
+    count += 1
+    if(count == x):
+        print('%s' % i)
+        count = 0
+    else:
+        print('%s ' % i, end='')
