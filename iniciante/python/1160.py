1139af819a392ca63ea838844c13161e8ee37125
quatroka <monteiro.leonardosantos@gmail.com>
2016-09-21 08:23:19 -0300

    Python -> Iniciante -> 1160 -> Completado

---
+++ iniciante/python/1160.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+t = int(input())
+
+for i in range(0, t):
+    line = input().split()
+    pa = int(line[0])
+    pb = int(line[1])
+    g1 = float(line[2])
+    g2 = float(line[3])
+
+    anos = 0
+    while pa <= pb:
+        pa = int(pa * ((g1 / 100) + 1))
+        pb = int(pb * ((g2 / 100) + 1))
+        anos += 1
+        if anos > 100:
+            break
+
+    if anos > 100:
+        print('Mais de 1 seculo.')
+    else:
+        print('%s anos.' % anos)
