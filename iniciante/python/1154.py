703d13f4be07a9d0e3e0c2cf1afb295c97a460f7
quatroka <monteiro.leonardosantos@gmail.com>
2016-09-21 00:51:22 -0300

    Python -> Iniciante -> 1154 -> Completado

---
+++ iniciante/python/1154.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+media = 0
+cont = 0
+
+while True:
+    n = int(input())
+
+    if n < 0:
+        break
+    else:
+        cont += 1
+        media += n
+
+print('%.2f' % (media / cont))
