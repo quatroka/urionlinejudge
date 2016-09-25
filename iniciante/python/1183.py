66dde6e52a17566e18cc485af830f2ce5dcecdf3
quatroka <monteiro.leonardosantos@gmail.com>
2016-09-25 03:14:34 -0300

    Python -> Iniciante -> 1183 -> Completado

---
+++ iniciante/python/1183.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+operation = input()
+
+matriz = []
+
+for i in range(0, 12):
+    matriz_aux = []
+
+    for j in range(0, 12):
+        matriz_aux.append(float(input()))
+
+    matriz.append(matriz_aux)
+
+soma = 0
+elementos = 0
+for k in range(0, len(matriz)):
+    for l in range(k + 1, len(matriz)):
+        soma += matriz[k][l]
+        elementos += 1
+
+
+if operation == 'S':
+    print('%.1f' % soma)
+else:
+    print('%.1f' % (soma / elementos))
