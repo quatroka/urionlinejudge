5ef6243262430fe66bfdd0dd8dd382c62a1ae7c8
quatroka <monteiro.leonardosantos@gmail.com>
2017-02-22 21:05:10 -0300

    Python -> Iniciante -> 2334 -> Completado

---
+++ iniciante/python/2334.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+while True:
+    n = int(input())
+    if n > 0:
+        print(n - 1)
+    elif n == 0:
+        print(0)
+    else:
+        break
